2259d65adbea0cd41b89baf3b5b457233ee3abc9
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-06-28 22:33:51 -0700

    Time: 9 ms (99.58%), Space: 43.3 MB (20.43%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,5 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
-        // char[] n = ("" + x).toCharArray();
-        // int l = n.length - 1;
-        // for (int i = 0; i < n.length/2; i++) {
-        //     if (n[i] != n[l-i]) {
-        //         return false;
-        //     }
-        // }
-        // return true;
         int c = x;
         int r = 0;
         int ten = 10;
