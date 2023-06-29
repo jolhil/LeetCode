561ae4a2175e897ef5e4510f8ec323de19a09473
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-06-28 22:34:14 -0700

    Time: 9 ms (99.58%), Space: 43.1 MB (37.06%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isPalindrome(int x) {
         int c = x;
         int r = 0;
-        int ten = 10;
+        // int ten = 10;
         
         while(c > 0) {
-            r = r*ten + c%ten;
-            c /= ten;
+            r = r*10 + c%10;
+            c /= 10;
         }
         return r == x;
         
