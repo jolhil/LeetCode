650d1cf4d55187b61129a4f360acc22288696c77
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-06-29 23:34:45 -0700

    Time: 4 ms (94.71%), Space: 43.9 MB (25.52%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
+        int n = 0;
         int ans = 0;
         int prev = 0;
-        
-        Map<Character, Integer> romanIntMap = new HashMap<>();
-        romanIntMap.put('I', 1);
-        romanIntMap.put('V', 5);
-        romanIntMap.put('X', 10);
-        romanIntMap.put('L', 50);
-        romanIntMap.put('C', 100);
-        romanIntMap.put('D', 500);
-        romanIntMap.put('M', 1000);
-        
-        for (int i = s.length()-1; i >= 0; i--) {
-            char c = s.charAt(i);
-            int n = romanIntMap.get(c);
-            
+
+        for (int i = s.length()-1; i >=0; i--) {
+            switch(s.charAt(i)) {
+                case 'M' -> n = 1000;
+                case 'D' -> n = 500;
+                case 'C' -> n = 100; 
+                case 'L' -> n = 50; 
+                case 'X' -> n = 10; 
+                case 'V' -> n = 5; 
+                case 'I' -> n = 1; 
+            }
             if (n < prev) {
                 ans -= n;
+
             } else {
                 ans += n;
             }
