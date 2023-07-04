2a9785585b4fb629e1966f59cf7eaa2bb0b15cf5
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-07-03 22:08:35 -0700

    Time: 2 ms (91.71%), Space: 40.8 MB (40.98%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,32 +4,29 @@ class Solution {
         Stack<Character> stack = new Stack<Character>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+                continue;
+            } else if (stack.isEmpty()) {
+                return false;
+            }
             switch (c) {
-                case '(':
-                    stack.push(c);
-                    break;
-                case '{':
-                    stack.push(c);
-                    break;
-                case '[':
-                    stack.push(c);
-                    break;
                 case ')':
-                    if (!stack.isEmpty() && stack.peek() =='(') {
+                    if (stack.peek() =='(') {
                         stack.pop();
                         break;
                     } else {
                         return false;
                     }
                 case '}':
-                    if(!stack.isEmpty() && stack.peek() == '{') {
+                    if(stack.peek() == '{') {
                         stack.pop();
                         break;
                     } else {
                         return false;
                     }
                 case ']':
-                    if(!stack.isEmpty() && stack.peek() == '[') {
+                    if(stack.peek() == '[') {
                         stack.pop();
                         break;
                     } else {
