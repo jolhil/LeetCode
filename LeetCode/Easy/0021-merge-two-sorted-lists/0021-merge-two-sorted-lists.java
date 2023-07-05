1670c456f743bc651e5ab40f5644a53d96c452bb
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-07-05 13:34:18 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (33.10%) - LeetHub

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,6 +10,30 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        /** Iterative solution.*/
+//         ListNode head = new ListNode();
+//         ListNode merged = head;
+        
+//         while (list1 != null && list2 != null) {
+//             if (list1.val <= list2.val) {
+//                 merged.next = list1;
+//                 list1 = list1.next;
+//             } else {
+//                 merged.next = list2;
+//                 list2 = list2.next;
+//             }
+//             merged = merged.next;
+//         }
+        
+//         if (list1 != null) {
+//             merged.next = list1;
+//         } else {
+//             merged.next = list2;
+//         }
+        
+//         return head.next;
+        
+        /** Recursive solution.*/
         if (list1 == null) return list2;
         if (list2 == null) return list1;
         ListNode merged;
