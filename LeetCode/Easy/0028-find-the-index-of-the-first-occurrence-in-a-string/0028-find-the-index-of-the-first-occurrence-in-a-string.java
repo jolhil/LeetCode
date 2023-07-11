9a7ca24038a9259cbd00c8185b7c7c930066ece8
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-07-10 22:58:00 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (80.08%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,11 +1,23 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        for (int i = 0;; i++) {
-            for (int j = 0;; j++) {
-                if (j == needle.length()) return i;
-                if (i + j == haystack.length()) return -1;
-                if (needle.charAt(j) != haystack.charAt(i+j)) break;
+        if (needle.isEmpty()) return 0;
+        int[] lps = computeKMPTable(needle);
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) j = lps[j - 1];
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                if (++j == m) return i - m + 1; // found solution
             }
         }
+        return -1;
+    }
+    private int[] computeKMPTable(String pattern) {
+        int n = pattern.length();
+        int[] lps = new int[n];
+        for (int i = 1, j = 0; i < n; i++) {
+            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) j = lps[j - 1];
+            if (pattern.charAt(i) == pattern.charAt(j)) lps[i] = ++j;
+        }
+        return lps;
     }
 }
