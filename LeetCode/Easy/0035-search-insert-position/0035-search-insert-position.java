c0c9b31c0e6442f6b3cc4c28a376133485c68d48
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-07-14 21:05:15 -0700

    Time: 0 ms (100.00%), Space: 43.2 MB (80.46%) - LeetHub

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.java
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.java
@@ -7,22 +7,12 @@ class Solution {
             if (nums[m] == target) {
                 return m;
             } else if (nums[m] < target) {
-                if (l == r) {
-                    return nums.length;
-                } else if (nums[m+1] > target) {
-                    return m  + 1;
-                } 
                 l = m + 1;
             } else {
-                if (m == 0) {
-                    return 0;
-                } else if (nums[m-1] < target) {
-                    return m;
-                }
                 r = m -1;
             }
         }
         
-        return 0;
+        return l;
     }
 }
