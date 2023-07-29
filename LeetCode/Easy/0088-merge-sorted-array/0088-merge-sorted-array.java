253144a617572713595b744376b890c1773b84d3
Junsang(Jason) Yoo <78401954+jolhil@users.noreply.github.com>
2023-07-28 23:23:04 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (28.49%) - LeetHub

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,7 +1,12 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Begin with the largest values of two arrays to be compared
+        // which avoid extra manipulation for the nums1 values.
+        
+        /** i for the index of the largest value of the nums1, j for the nums2, k for the final nums1 */
         int i = m - 1, j = n - 1, k = m + n - 1;
         
+        // Using nums2 index for the while loop since nums1 values already in the nums1
         while(j >= 0) {
             if(i >= 0)
                 nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
